f9604e92b917dc66710684dcb90246585b356559
latorreandrea <latorre.andrea.93@gmail.com>
2021-06-09 20:42:36 +0000

    set the description of the function that we need to play

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//create an event listener that start game by pressing button StartGame!
+document.getElementsByClassName("start").addEventListener("click", startGame())
+
+//set a start game function that play initial animation and shows game area
+
+startGame(){
+    
+}
+
+//create a function that randomly showing a mole
+
+//create a function that confirm we hit the mole
+
+//create a function that increase combo
+
+//create a function that decrese the attempts and stop the game if we finish them
