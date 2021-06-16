9411e59b0823093872077d3a38a31234c22e04ff
latorreandrea <latorre.andrea.93@gmail.com>
2021-06-16 19:54:00 +0000

    create a function that hide moles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,52 @@
 //create an event listener that start game by pressing button StartGame!
 document.getElementsByClassName("start")[0].addEventListener('click', startGame);
 
+let num = Math.floor(Math.random()*3)+1;            
+let showA = document.getElementsByTagName('img')[0];
+let showS = document.getElementsByTagName('img')[1];
+let showD = document.getElementsByTagName('img')[2];
+
 //set a start game function that play initial animation and shows game area
 
 function startGame(event) {
     document.getElementById('game-area').style.display = "block";
     document.getElementById('combo').style.display = "block";
     document.getElementById('loading').style.display = "none";
+    setInterval(() => {
+    showMole();
+}, 1500)
+
 }
 
 //create a function that randomly showing a mole
+
 function showMole() {
-
-let num = Math.floor(Math.random()*3)+1;
- 
-
+    num = Math.floor(Math.random()*3)+1;        
 if (num == 1){
-        document.getElementsByTagName('img')[0].src = "./assets/images/talpaesce.svg";        
+        showA.src = "./assets/images/talpaesce.svg"; 
+        setTimeout("hideMole()", 1000);               
     }else if(num == 2){
-        document.getElementsByTagName('img')[1].src = "./assets/images/talpaesce.svg";
+        showS.src = "./assets/images/talpaesce.svg";
+        setTimeout("hideMole()", 1000);
     }else if(num == 3){
-        document.getElementsByTagName('img')[2].src = "./assets/images/talpaesce.svg";
+        showD.src = "./assets/images/talpaesce.svg";
+        setTimeout("hideMole()", 1000);
     }
 }
-setInterval(() => {
-    showMole();
-}, 2000)
+
+function hideMole() {
+    
+    if (num == 1){
+        showA.src = "./assets/images/terreno3.svg";                
+    }else if(num == 2){
+        showS.src = "./assets/images/terreno3.svg";
+    }else if(num == 3){
+        showD.src = "./assets/images/terreno3.svg";
+    }
+
+}
 // create a function that hide mole after some time
+
 
 //create a function that confirm we hit the mole
 
